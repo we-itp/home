6954ecf292970f370cd4d341e1e1c2c57e00611b
Sue Roh Windows <sueroh@gmail.com>
2020-12-09 23:29:00 -0500

    Updates

---
+++ precache-manifest.24a185db47b5e541c613d6c52cd1507d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6657162f4ca0f74d3a31a94a81bb18f1",
+    "url": "/weitp/index.html"
+  },
+  {
+    "revision": "b7d4138a5a42c0703b39",
+    "url": "/weitp/static/css/main.363904d8.chunk.css"
+  },
+  {
+    "revision": "8b2d2d202d3dbc87f75e",
+    "url": "/weitp/static/js/2.ab974284.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weitp/static/js/2.ab974284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d4138a5a42c0703b39",
+    "url": "/weitp/static/js/main.a4fb4bc2.chunk.js"
+  },
+  {
+    "revision": "06aee29a82615eed009a",
+    "url": "/weitp/static/js/runtime-main.733ab795.js"
+  }
+]);
