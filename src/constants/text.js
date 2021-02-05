2bb9efedad0a6e3f2b2f168c93ba3d9af332ba1b
Katie Han <katie@normalvr.com>
2021-02-05 16:24:20 -0500

    updated text 2/5 meeting

--- src/constants/text.js
+++ src/constants/text.js
@@ -14,7 +14,7 @@ const HomeText = [
     ],
     [
         {
-            text: 'We’ve since begun developing programming and initiatives aligned with these goals and values:',
+            text: 'We have developed programming and initiatives aligned with these goals and values:',
             className: 'home-boldsubtext'
         },
         {
@@ -29,36 +29,24 @@ const HomeText = [
 ]
 
 const AboutText = [
-    'WE @ ITP began over the summer lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'Our mission is to lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'Explanation about ITP: lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'We see ourselves as different from tisch career offices because lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    'WE @ ITP began as an effort to both better understand the workforce that we as ITP students would be entering into, and to create an environment at ITP where people can share experiences and advice that could help us as a collective.',
+    'Our mission is to destigmatize open conversation around salaries and work experience and gain exposure to a variety of career paths.',
+    `The ITP (Interactive Telecommunications Program) at NYU Tisch is a graduate program that focuses on the imaginative and experimental use of communications technologies — how they might augment, improve, and bring delight and art into people's lives. Given the interdisciplinary nature of the program, WE @ ITP aims to provide specialized guidance and mentorship for various career paths that may branch out of ITP.`,
+    'We are working in a partnership with the Tisch Career offices to help them better understand the uniquely wide range of careers that ITP students will be applying for. We merged efforts on a mentorship program that will better serve and equip ITP graduates alongside their colleagues in other Tisch programs.',
 ]
 
 const ResourcesText = [
     [
         {
             title: 'Tisch Career Center',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            text: 'The Tisch Career Center offers programming, counseling, and career-related resources for artists and cultural innovators. They educate and advise students on how to translate creative skills into professional experiences. Services include resume/cover letter review, one-one coaching, and group workshops.'
         },
         {
             title: 'Wasserman Center',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        },
-        {
-            title: 'Interview Help',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            text: 'The Wasserman Center at NYU offers general career coaching to current and graduated NYU students (up to 3 years after graduating) through scheduled 30 minute on-on-one appointments. They also provide industry insights, a digital networking platform called the Violet Network and internship and job searching on Handshake.'
         }
     ],
     [
-        {
-            title: 'How to approach wage negotiations',
-            text: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        },
-        {
-            title: 'How to approach wage negotiations',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        }
     ]
 ]
 
