aac05b9e074fc20b142722bf0b5b1002d599f20b
Katie Han <katie@normalvr.com>
2021-02-17 22:07:05 -0500

    copy update and mobile styles

--- src/constants/text.js
+++ src/constants/text.js
@@ -2,37 +2,28 @@
 const ContactText = [
     { text: 'Email us at ', email: 'wageequity@itp.nyu.edu' },
     { text: 'Or reach out to any of our current members! Our current members include:' },
-    { text: 'Dawn Sinkowski, Lizzy Chiappini, Simone Salvo, Sydney Meyers, Katie Han, Sue Roh, Na Chen, Christina Dacanay, and Ji Park' }
+    { text: 'Na Chen, Lizzy Chiappini, Christina Dacanay, Katie Han, Sydney Meyers, Ji Park, Sue Roh, Simone Salvo, and Dawn Sinkowski' }
 ]
 
 const HomeText = [
     [
         {
-            text: 'Wage Equity @ ITP is a working group with a shared interest in destigmatizing open conversations around salaries and work experiences.',
+            text: 'Wage Equity @ ITP is a student-run working group that focuses on shifting how we talk about careers, salaries, and workplaces.',
             className: ''
         }
     ],
     [
         {
-            text: 'We have developed programming and initiatives aligned with these goals and values:',
-            className: 'home-boldsubtext'
-        },
-        {
-            text: 'A weekly speaker series that features an invited guest working in an ITP-adjacent industry to share their professional trajectories and offer candid insights based on their personal experiences',
-            className: 'home-subtext'
-        },
-        {
-            text: 'A mentorship program for graduating second-years to develop meaningful connections with ITP alumni, supporting a constructive transition from the ITP student community to a broader professional field',
+            text: 'In an aim to bring more transparency and authenticity to career development, we host conversations with featured guests working in ITP-adjacent industries to candidly share their professional journeys and experiences. ITP is a community of many interests: we’re looking to start businesses, sustain art practices, teach, and work at agencies, start-ups, and nonprofits. Some are switching fields mid-career and some are entering the job market for the first time. WE @ ITP will continue to evolve to meet the needs of this expansive community.',
             className: 'home-subtext'
         }
     ]
 ]
 
 const AboutText = [
-    'WE @ ITP began as an effort to both better understand the workforce that we as ITP students would be entering into, and to create an environment at ITP where people can share experiences and advice that could help us as a collective.',
-    'Our mission is to destigmatize open conversation around salaries and work experience and gain exposure to a variety of career paths.',
-    `The ITP (Interactive Telecommunications Program) at NYU Tisch is a graduate program that focuses on the imaginative and experimental use of communications technologies — how they might augment, improve, and bring delight and art into people's lives. Given the interdisciplinary nature of the program, WE @ ITP aims to provide specialized guidance and mentorship for various career paths that may branch out of ITP.`,
-    'We are working in a partnership with the Tisch Career offices to help them better understand the uniquely wide range of careers that ITP students will be applying for. We merged efforts on a mentorship program that will better serve and equip ITP graduates alongside their colleagues in other Tisch programs.',
+    'Our mission is to destigmatize conversations around salaries and work experience, while gaining exposure to a variety of career paths.',
+    `The Interactive Telecommunications Program (ITP) at NYU Tisch is a graduate program that focuses on the imaginative and experimental use of communications technologies—how they might augment, impact, and bring delight and art into people's lives. Given the interdisciplinary nature of the program, WE @ ITP aims to provide specialized peer-to-peer guidance and support for exploring various career paths that may branch out of ITP.`,
+    `We developed a mentorship model to connect graduating students with ITP alumni for long-term one-on-one support, which has now merged with a new program run by the Tisch Student Affairs office. Participating students across Tisch will be matched with mentors according to areas of interest.`,
 ]
 
 const ResourcesText = [
@@ -43,10 +34,8 @@ const ResourcesText = [
         },
         {
             title: 'Wasserman Center',
-            text: 'The Wasserman Center at NYU offers general career coaching to current and graduated NYU students (up to 3 years after graduating) through scheduled 30 minute on-on-one appointments. They also provide industry insights, a digital networking platform called the Violet Network and internship and job searching on Handshake.'
+            text: 'The Wasserman Center at NYU offers general career coaching to current and graduated NYU students (up to 3 years after graduating) through scheduled 30 minute one-on-one appointments. They also provide industry insights, a digital networking platform called the Violet Network and internship and job searching on Handshake.'
         }
-    ],
-    [
     ]
 ]
 
